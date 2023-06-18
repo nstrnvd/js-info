6a115c593d04f28a78529ccb5d89626160a1692c
Nastaran Valizadeh <nastaranvalizade2@gmail.com>
2023-06-18 17:06:02 +0330

    object.js added

---
+++ src/object.js
@@ -0,0 +1,51 @@
+// let user = {};
+// user.name = "John";
+// user.surname = "Smith";
+// user.name = "Pete";
+// delete user.name;
+
+
+// let obj1 = {};
+// isEmpty(obj1);
+// function isEmpty(a) {
+//   for (let i in a) {
+//     // if the loop has started, there is a property
+//     return false;
+//   }
+//   return true;
+// }
+
+
+// let salaries = {
+//   John: 100,
+//   Ann: 160,
+//   Pete: 130
+// }
+
+// sum(salaries);
+// function sum(obj) {
+//   let result = 0;
+//     for(let key in obj) {
+//       result = result + obj[key];
+//     }
+//     obj.sum = result;
+// }
+
+// let menu = {
+//   width: 200,
+//   height: 300,
+//   title: "My menu"
+// };
+// multiplyNumeric(menu);
+// function multiplyNumeric(menu) {
+//   for(let key in menu) {
+//     if (typeof menu[key] === 'number') {
+//       menu[key] *=2;
+//     }
+//   }
+//   console.log(menu);
+// }
+
+
+
+
